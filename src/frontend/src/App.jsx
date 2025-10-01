ee1ea0d3f5e9c84400ea50f0816eacfb3c79abde
Minh Coffee <hominhlanhat@gmail.com>
2025-10-01 15:47:33 +0700

    updateRegister

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -8,11 +8,8 @@ import AuthLayout from "./layout/authenLayout";
 import { Toaster } from "react-hot-toast";
 import PublicRoute from "./routes/PublicRoute";
 import ForgotPasswordPage from "./pages/forgotpasswordPage/ForgotPasswordPage";
-<<<<<<< HEAD
 import VerifyEmailPage from "./pages/VerifyEmailPage/VerifyEmailPage";
-=======
-import MainLayout from "./layout/mainLayout";
->>>>>>> 2c6ec63bddf397448c92ba2c5bb3b12dd8f5d8cf
+
 function App() {
   const router = createBrowserRouter([
     {
