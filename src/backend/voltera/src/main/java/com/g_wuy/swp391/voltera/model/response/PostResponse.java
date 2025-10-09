7d8819d3c359555fa130ab9413e2963af97b00c1
vinh-sudo <vbui3168@gmail.com>
2025-10-09 20:44:16 +0700

    Refresh token and update login

--- src/backend/voltera/src/main/java/com/g_wuy/swp391/voltera/model/response/PostResponse.java
+++ src/backend/voltera/src/main/java/com/g_wuy/swp391/voltera/model/response/PostResponse.java
@@ -3,8 +3,8 @@ package com.g_wuy.swp391.voltera.model.response;
 import java.math.BigDecimal;
 import java.util.List;
 
-import com.g_wuy.swp391.voltera.model.dto.BatteryDto;
-import com.g_wuy.swp391.voltera.model.dto.VehicleDto;
+import com.g_wuy.swp391.voltera.model.dto.BatteryDTO;
+import com.g_wuy.swp391.voltera.model.dto.VehicleDTO;
 
 import lombok.Data;
 
@@ -15,7 +15,7 @@ public class PostResponse {
     private String description;
     private BigDecimal price;
     private String status;
-    private BatteryDto battery;
-    private VehicleDto vehicle;
+    private BatteryDTO battery;
+    private VehicleDTO vehicle;
     private List<String> imageUrls;
 }
